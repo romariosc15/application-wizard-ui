023ac985cbd4f50373213abbd7cd894ddc7b18b5
romariosc15 <romariosc15@outlook.com>
2022-09-07 19:05:13 -0500

    Consuming Contentful API.

--- src/pages/wizard/[id].tsx
+++ src/pages/wizard/[id].tsx
@@ -1,5 +1,7 @@
 import { WizardContextProvider } from '../../context/WizardContext';
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Layout from '../../components/Layout';
@@ -7,43 +9,83 @@ import Steps from '../../components/Steps';
 import WizardForm from '../../components/Wizard/Form';
 import WizardSuccess from '../../components/Wizard/Success';
 import WizardError from '../../components/Wizard/Error';
-import wizardDetailData from '../../data/WizardDetail';
+import Spinner from '../../components/Spinner';
 
 const Wizard: NextPage = () => {
   const router = useRouter();
   const id = (router.query.id)?.toString() || '';
-  const steps:[] = wizardDetailData[id] || [];
+  const query = `
+  {
+    applicationForm(id: "${id}") {
+        title
+        description
+        author
+        logo {
+          url
+        }
+        steps
+    }
+  }
+  `;
+  const [applicationForm, setApplicationForm] = useState({steps: []});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getApplicationForm = async () => {
+      try {
+        const response = await axios.post(`https://graphql.contentful.com/content/v1/spaces/${process.env.contentfulSpace}/`, { query }, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: process.env.contentfulToken || "",
+          }
+        });
+        setApplicationForm(response.data.data.applicationForm);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if(id){
+      getApplicationForm();
+    }
+  }, [id]);
   return (
     <Layout>
       <Head>
         <title>Asistente - Postulación</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="py-4 px-12 bg-gray-50 default-min-screen">
-        <WizardContextProvider>
-          <div className='w-6/12 mx-auto py-16'>
-            <Steps
-              steps={steps}
-            />
-            {
-              steps.map((value:any, index:number) => (
-                <div className="mt-5 md:mt-0 md:col-span-2" key={index}>
-                  <WizardForm
-                    title={value.title}
-                    order={value.order}
-                    fields={value.fields}
-                    stepsLength={steps.length}
-                  />
-                </div>
-              ))
-            }
-            {
-              steps.length > 0 ? <WizardSuccess stepsLength={steps.length} />
-               : <WizardError stepsLength={steps.length} />
-            }
-          </div>
+      <div className="py-4 px-12 bg-gray-50 default-min-screen flex justify-center items-center">
+        {
+          loading ? <Spinner /> :
+          <WizardContextProvider>
+            <div className='w-6/12 mx-auto'>
+              <Steps
+                steps={applicationForm.steps}
+              />
+              {
+                applicationForm.steps.map((value:any, index:number) => (
+                  <div className="mt-5 md:mt-0 md:col-span-2" key={index}>
+                    <WizardForm
+                      title={value.title}
+                      order={value.order}
+                      fields={value.fields}
+                      stepsLength={applicationForm.steps.length}
+                    />
+                  </div>
+                ))
+              }
+              {
+                applicationForm.steps.length > 0 && <WizardSuccess stepsLength={applicationForm.steps.length} />
+              }
+              {
+                error && <WizardError stepsLength={applicationForm.steps.length} />
+              }
+            </div>
 
-        </WizardContextProvider>
+          </WizardContextProvider>
+        }
       </div>
     </Layout>
   );
--- src/pages/wizard/index.tsx
+++ src/pages/wizard/index.tsx
@@ -1,38 +1,80 @@
 import type { NextPage } from 'next';
-import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Layout from '../../components/Layout';
+import Spinner from '../../components/Spinner';
 import Head from 'next/head';
 import Link from 'next/link';
-import wizardData from '../../data/Wizard';
 
 const WizardIndex: NextPage = () => {
+    const query = `
+    {
+        applicationFormCollection {
+            items {
+            sys{
+                id
+            }
+            title
+            description
+            author
+            logo {
+                url
+            }
+            }
+        }
+    }
+    `;
+    const [applicationForms, setApplicationForms] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const getApplicationForms = async () => {
+        try {
+            const response = await axios.post(`https://graphql.contentful.com/content/v1/spaces/${process.env.contentfulSpace}/`, { query }, {
+                headers: {
+                "Content-Type": "application/json",
+                Authorization: process.env.contentfulToken || "",
+                }
+            });
+            setApplicationForms(response.data.data.applicationFormCollection.items);
+        } catch (error) {
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+        };
+        getApplicationForms();
+    }, []);
     return (
         <Layout>
             <Head>
                 <title>Asistente - Formularios</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <div className="py-12 px-6 2xl:px-12 bg-gray-50 default-min-screen">
-                <div className="grid grid-cols-1 2xl:grid-cols-4 space-y-6 lg:space-y-0 lg:space-x-6">
-                    {
-                        wizardData.map((value:any, index:number) => (
-                            <div className="shadow rounded-md sm:overflow-hidden bg-white" key={index}>
-                                <div className='px-6 py-6'>
-                                    <div className="flex flex-row justify-between items-center">
-                                        <h5 className="text-lg 2xl:text-2xl font-medium">
-                                            <Link href={`/wizard/${value.id}`}>
-                                                {value.title}
-                                            </Link>
-                                        </h5>
-                                        <img src={value.logo} alt="me" className="w-12 2xl:w-16" />
+            <div className={`py-12 px-6 2xl:px-12 bg-gray-50 default-min-screen ${loading && "flex justify-center items-center"}`}>
+                {
+                    loading ? <Spinner /> :
+                    <div className="grid grid-cols-1 2xl:grid-cols-4 space-y-6 lg:space-y-0 lg:space-x-6">
+                        {
+                            applicationForms.map((value:any, index:number) => (
+                                <div className="shadow rounded-md sm:overflow-hidden bg-white" key={index}>
+                                    <div className='px-6 py-6'>
+                                        <div className="flex flex-row justify-between items-center">
+                                            <h5 className="text-lg 2xl:text-2xl font-medium">
+                                                <Link href={`/wizard/${value.sys.id}`}>
+                                                    {value.title}
+                                                </Link>
+                                            </h5>
+                                            <img src={value.logo.url} alt="me" className="w-12 2xl:w-16" />
+                                        </div>
+                                        <p className="text-sm 2xl:text-base text-gray-500">{value.author}</p>
+                                        <p className="text-sm 2xl:text-base font-light mt-2">{`${value.description}`}</p>
                                     </div>
-                                    <p className="text-sm 2xl:text-base text-gray-500">{value.author}</p>
-                                    <p className="text-sm 2xl:text-base font-light mt-2">{`${value.description}`}</p>
                                 </div>
-                            </div>
-                        ))
-                    }
-                </div>
+                            ))
+                        }
+                    </div>
+                }
             </div>
         </Layout>
     );
